0dcaa257bbf057cb1712747c19438429a0e4df93
korlir <anonym.show@mail.ru>
2023-04-10 18:13:40 +0300

    Add coding tree

---
+++ internal/compressor/coding_tree/coding_tree.go
@@ -0,0 +1,90 @@
+package coding_tree
+
+import (
+	"archiver/internal/compressor/dictionaries"
+	"archiver/pkg/map_sort"
+	"fmt"
+	"sort"
+)
+
+type CodingTree node
+
+type node struct {
+	leftChild  *node
+	rightChild *node
+
+	value int32
+	freq  int64
+}
+
+func GenerateCodingTree(dict dictionaries.FreqDict) CodingTree {
+
+	if len(dict) == 0 {
+		return CodingTree{}
+	}
+
+	sortedKeys := map_sort.SortMapByValues(dict, true)
+
+	nodes := make([]node, len(sortedKeys))
+
+	// creating base free nodes tree
+	for index := range nodes {
+		nodes[index] = node{leftChild: nil, rightChild: nil, value: sortedKeys[index], freq: dict[sortedKeys[index]]}
+	}
+
+	for len(nodes) > 1 {
+		// merge nodes with the lowest freq
+		parentNode := node{leftChild: &nodes[0], rightChild: &nodes[1], freq: nodes[0].freq + nodes[1].freq}
+
+		// remove child nodes
+		nodes = nodes[2:]
+
+		// add parent nodes
+		nodes = append(nodes, parentNode)
+
+		// resort all nodes
+		sort.Slice(nodes, func(i, j int) bool {
+			return nodes[i].freq < nodes[j].freq
+		})
+	}
+
+	return CodingTree(nodes[0])
+}
+
+func IsTreeEquals(tree *node, tree1 *node) bool {
+	if tree == nil && tree1 != nil || tree != nil && tree1 == nil {
+		return false
+	}
+
+	// mean both of them
+	if tree == nil {
+		return true
+	}
+
+	if tree.freq != tree1.freq || tree.value != tree1.value {
+		return false
+	}
+
+	return IsTreeEquals(tree.leftChild, tree1.leftChild) && IsTreeEquals(tree.rightChild, tree1.rightChild)
+}
+
+func PrintTree(tree CodingTree) {
+	dfsPrint((*node)(&tree), "", "", "")
+	fmt.Println()
+}
+
+func dfsPrint(node *node, rpref string, cpref string, lpref string) {
+	if node == nil {
+		return
+	}
+
+	if node.rightChild != nil {
+		dfsPrint(node.rightChild, rpref+"  ", rpref+"/--", rpref+"|  ")
+	}
+
+	fmt.Printf(cpref+"(f: %d v: %d)\n", node.freq, node.value)
+
+	if node.leftChild != nil {
+		dfsPrint(node.leftChild, lpref+"|  ", lpref+"\\--", lpref+"   ")
+	}
+}
---
+++ internal/compressor/coding_tree/coding_tree_test.go
@@ -0,0 +1,134 @@
+package coding_tree
+
+import (
+	"archiver/internal/compressor/dictionaries"
+	"fmt"
+	"testing"
+)
+
+func TestGenerateCodingTree(t *testing.T) {
+	t.Log("Start checking generating coding tree")
+	{
+		testID := 0
+
+		t.Logf("\tTest %d: check generating tree with even count of nodes", testID)
+		{
+			dict := dictionaries.FreqDict{5: 2, 0: 8, 4: 4, 7: 1} // sorted:  7(1) -> 5(2) -> 4(4) -> 0(8)
+			tree := GenerateCodingTree(dict)
+
+			expectedTree := CodingTree(node{
+				freq: 15,
+				leftChild: &node{
+					freq: 7,
+					leftChild: &node{
+						freq: 3,
+						leftChild: &node{
+							freq:  1,
+							value: 7,
+						},
+						rightChild: &node{
+							freq:  2,
+							value: 5,
+						},
+					},
+					rightChild: &node{
+						freq:  4,
+						value: 4,
+					},
+				},
+				rightChild: &node{
+					freq:  8,
+					value: 0,
+				},
+			})
+
+			if !IsTreeEquals((*node)(&tree), (*node)(&expectedTree)) {
+				t.Logf("\tFail on test %d. Incorrect tree. Expected another. Check trees printing", testID)
+				fmt.Println("Got:")
+				PrintTree(tree)
+				fmt.Println("Expected:")
+				PrintTree(expectedTree)
+				t.Fail()
+			}
+
+		}
+		testID++
+
+		t.Logf("\tTest %d: check generating tree with odd count of nodes", testID)
+		{
+			dict := dictionaries.FreqDict{20: 5, 5: 0, 30: 60} // sorted:  5(0) -> 20(5) -> 30(60)
+			tree := GenerateCodingTree(dict)
+
+			expectedTree := CodingTree(node{
+				freq: 65,
+				leftChild: &node{
+					freq: 5,
+					leftChild: &node{
+						freq:  0,
+						value: 5,
+					},
+					rightChild: &node{
+						freq:  5,
+						value: 20,
+					},
+				},
+				rightChild: &node{
+					freq:  60,
+					value: 30,
+				},
+			})
+
+			if !IsTreeEquals((*node)(&tree), (*node)(&expectedTree)) {
+				t.Logf("\tFail on test %d. Incorrect tree. Expected another. Check trees printing", testID)
+				fmt.Println("Got:")
+				PrintTree(tree)
+				fmt.Println("Expected:")
+				PrintTree(expectedTree)
+				t.Fail()
+			}
+
+		}
+		testID++
+
+		t.Logf("\tTest %d: check generating tree with one node", testID)
+		{
+			dict := dictionaries.FreqDict{20: 5}
+			tree := GenerateCodingTree(dict)
+
+			expectedTree := CodingTree(node{
+				freq:  5,
+				value: 20,
+			})
+
+			if !IsTreeEquals((*node)(&tree), (*node)(&expectedTree)) {
+				t.Logf("\tFail on test %d. Incorrect tree. Expected another. Check trees printing", testID)
+				fmt.Println("Got:")
+				PrintTree(tree)
+				fmt.Println("Expected:")
+				PrintTree(expectedTree)
+				t.Fail()
+			}
+
+		}
+		testID++
+
+		t.Logf("\tTest %d: check generating tree with none node", testID)
+		{
+			dict := dictionaries.FreqDict{}
+			tree := GenerateCodingTree(dict)
+
+			expectedTree := CodingTree(node{})
+
+			if !IsTreeEquals((*node)(&tree), (*node)(&expectedTree)) {
+				t.Logf("\tFail on test %d. Incorrect tree. Expected another. Check trees printing", testID)
+				fmt.Println("Got:")
+				PrintTree(tree)
+				fmt.Println("Expected:")
+				PrintTree(expectedTree)
+				t.Fail()
+			}
+
+		}
+		testID++
+	}
+}

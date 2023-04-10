e04b392620be2610a59ff7f37c640d629fa74c7f
korlir <anonym.show@mail.ru>
2023-04-10 03:00:01 +0300

    Fix sort

--- pkg/map_sort/map_sort.go
+++ pkg/map_sort/map_sort.go
@@ -1,10 +1,17 @@
-package pkg
+package map_sort
 
 import "sort"
 
+type ordered interface {
+	int | int8 | int16 | int32 | int64 |
+	uint | uint8 | uint16 | uint32 | uint64 |
+	float32 | float64 |
+	string | uintptr
+}
+
 // SortMapByValues sort map by values in some order (use isAscending for choosing). Sort is stable.
 // Return sorted keys
-func SortMapByValues[K comparable, V comparable](m map[K]V, isAscending bool) []K {
+func SortMapByValues[K comparable, V ordered](m map[K]V, isAscending bool) []K {
 	keys := make([]K, 0)
 
 	for key := range m {
@@ -25,7 +32,7 @@ func SortMapByValues[K comparable, V comparable](m map[K]V, isAscending bool) []
 
 // SortMapByKeys sort map by keys in some order (use isAscending for choosing). Sort is stable.
 // Return sorted keys
-func SortMapByKeys[K comparable, V any](m map[K]V, isAscending bool) []K {
+func SortMapByKeys[K ordered, V any](m map[K]V, isAscending bool) []K {
 	keys := make([]K, 0)
 
 	for key := range m {
